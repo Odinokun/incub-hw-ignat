d34e5784530810a3b27a702e2f730726e1b2d7a9
odinokun <odinokun@gmail.com>
2022-12-06 12:16:00 +0100

    commit

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -22,7 +22,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         //         ? ...
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (className ? ' ' + className : '')
 
     return (
         <button
